3f8b52a7305b0ef3367c3598bd3797f63b765491
Ammar Mirkar <69316553+Ammar-Mirkar@users.noreply.github.com>
2023-10-27 08:30:19 +0530

    Add files via upload
    This script can help create bulk mailboxes

---
+++ New-Mailboxes.ps1
@@ -0,0 +1,5 @@
+#Create bulk mailboxes in onprem
+
+$Password=Read-Host “Enter Password” –AsSecureString
+ 
+Import-CSV “C:\Users\mailboxes.csv” | ForEach {New-Mailbox -Alias $_.alias -FirstName $_.first -LastName $_.last -Name $_.name -userPrincipalName $_.upn -Database “database name here” -OrganizationalUnit "contoso.com/Region" -Password $Password}
